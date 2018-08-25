cb3443d409545f9f1b5ba2c4de2ab9d84241d305
SJD <szy@sunzhongyang.com>
2018-08-25 10:31:29 -0400

    init commit

---
+++ FCN-16s/infer.py
@@ -0,0 +1,42 @@
+import numpy as np
+from PIL import Image
+
+import matplotlib.pyplot as plt
+
+import caffe
+
+caffe.set_device(0)
+caffe.set_mode_gpu()
+
+np.set_printoptions(threshold=np.inf)
+
+# load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
+im = Image.open('example.jpg')
+in_ = np.array(im, dtype=np.float32)
+in_ -= in_.mean()
+
+# load net
+net = caffe.Net('deploy_17.prototxt', 'models/OCT.caffemodel', caffe.TEST)
+# shape for input (data blob is N x C x H x W), set data
+net.blobs['data'].reshape(1, 1, *in_.shape)
+net.blobs['data'].data[...] = in_
+# run net and take argmax for prediction
+net.forward()
+
+for i in range(0, 0):
+    tmp_out = net.blobs['upscore16_sem'].data[0][i]
+    plt.imshow(tmp_out, cmap='gray');
+    plt.axis('off')
+    plt.savefig('filters/' + str(i) + '.jpg')
+
+out = net.blobs['score_sem'].data[0].argmax(axis=0)
+
+[rows, cols] = out.shape;
+
+for countRow in range(rows - 1):
+	for countCol in range(cols - 1):
+		if out[countRow, countCol] == 1:
+			out[countRow, countCol] = 255;
+
+outImage = Image.fromarray(np.uint8(out));
+outImage.save('segmentation.jpg', 'JPEG');
---
+++ FCN-16s/models/model_readme.txt
@@ -0,0 +1,1 @@
+The model is too large to upload, please send e-mail to szy019@gmail.com to get the model, thankyou.
